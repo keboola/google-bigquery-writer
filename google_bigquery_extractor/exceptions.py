ad827b582f213aeb254821c83c2158e9012d2605
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-02-09 16:42:34 +0100

    coding style

--- google_bigquery_extractor/exceptions.py
+++ google_bigquery_extractor/exceptions.py
@@ -3,6 +3,7 @@ class UserException(Exception):
         Exception.__init__(self, message)
         self.message = message
 
+
 class ApplicationException(Exception):
     def __init__(self, message):
         Exception.__init__(self, message)
