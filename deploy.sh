ab146e4553617232b75209a9bc428b9b0dc82950
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-02-22 16:51:07 +0100

    fix(deploy): image names

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 # needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
 eval $(aws ecr get-login --region us-east-1)
-docker tag keboola/wr-google-bigquery:$TRAVIS_TAG 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/wr-google-bigquery:$TRAVIS_TAG
-docker tag keboola/wr-google-bigquery:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/wr-google-bigquery:latest
+docker tag keboola/google-bigquery-writer:$TRAVIS_TAG 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/wr-google-bigquery:$TRAVIS_TAG
+docker tag keboola/google-bigquery-writer:latest 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/wr-google-bigquery:latest
 docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/wr-google-bigquery:$TRAVIS_TAG
 docker push 147946154733.dkr.ecr.us-east-1.amazonaws.com/keboola/wr-google-bigquery:latest
