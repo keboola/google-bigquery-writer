87eb60b717b55bbccca91aff8c5dcd462580b347
David Střítežský <stritezskyd@gmail.com>
2018-07-17 10:00:49 +0200

    Split Writer into smaller methods, add strict types

--- google_bigquery_writer/writer.py
+++ google_bigquery_writer/writer.py
@@ -1,4 +1,4 @@
-from google.cloud import bigquery, exceptions as BQExceptions
+from google.cloud import bigquery, exceptions as bq_exceptions
 from google.auth import exceptions
 from google_bigquery_writer.exceptions import UserException
 from google_bigquery_writer import schema_mapper
@@ -6,12 +6,83 @@ import time
 
 
 class Writer(object):
-    def __init__(self, bigquery_client):
+    def __init__(self, bigquery_client: bigquery.Client):
         self.bigquery_client = bigquery_client
 
-    def write_table(self, csv_file_path, dataset_name, table_definition, columns_schema,
-                    incremental=False
-                    ):
+    def obtain_dataset(self, dataset_name: str) -> bigquery.Dataset:
+        dataset_reference = self.bigquery_client.dataset(dataset_name)
+
+        try:
+            return self.bigquery_client.get_dataset(dataset_reference)
+        except bq_exceptions.NotFound:
+            try:
+                dataset_obj = bigquery.Dataset(dataset_reference)
+                return self.bigquery_client.create_dataset(dataset_obj)
+            except bq_exceptions.NotFound:
+                message = 'Project %s was not found.' % (
+                    self.bigquery_client.project
+                )
+                raise UserException(message)
+        except exceptions.RefreshError:
+            message = 'Cannot connect to BigQuery.' \
+                      ' Check your access token or refresh token.'
+            raise UserException(message)
+        except bq_exceptions.BadRequest as err:
+            message = 'Cannot create dataset %s: %s' % (
+                dataset_name,
+                str(err)
+            )
+            raise UserException(message)
+
+    def prepare_table(
+            self,
+            dataset: bigquery.Dataset,
+            table_name: str,
+            columns_schema: list,
+            incremental: bool
+    ) -> bigquery.TableReference:
+        table_reference = dataset.table(table_name)
+        table = bigquery.Table(table_reference, columns_schema)
+
+        try:
+            bq_table = self.bigquery_client.get_table(table_reference)
+            table_exist = True
+            if incremental:
+                schema_mapper.is_table_definition_in_match_with_bigquery(
+                    columns_schema,
+                    bq_table
+                )
+            else:
+                self.bigquery_client.delete_table(table_reference)
+                table_exist = False
+        except bq_exceptions.NotFound:
+            table_exist = False
+        except bq_exceptions.BadRequest as err:
+            message = 'Cannot create table %s: %s' % (
+                table_reference,
+                str(err)
+            )
+            raise UserException(message)
+
+        if not table_exist:
+            try:
+                self.bigquery_client.create_table(table)
+            except bq_exceptions.BadRequest as err:
+                message = 'Cannot create table %s: %s' % (
+                    table_name,
+                    str(err)
+                )
+                raise UserException(message)
+        return table_reference
+
+    def write_table(
+            self,
+            csv_file_path: str,
+            dataset_name: str,
+            table_definition: dict,
+            columns_schema: list,
+            incremental: bool = False
+    ) -> bigquery.LoadJob:
         if dataset_name == '' or dataset_name is None:
             raise UserException('Dataset name not specified.')
         if table_definition['dbName'] == '' or table_definition['dbName'] is None:
@@ -21,62 +92,8 @@ class Writer(object):
 
         # TODO list projects and validate, that the project exists
 
-        dataset_reference = self.bigquery_client.dataset(dataset_name)
-
-        try:
-            dataset = self.bigquery_client.get_dataset(dataset_reference)
-        except BQExceptions.NotFound:
-            try:
-                dataset_obj = bigquery.Dataset(dataset_reference)
-                dataset = self.bigquery_client.create_dataset(dataset_obj)
-            except BQExceptions.NotFound:
-                message = 'Project %s was not found.' % (
-                    self.bigquery_client.project
-                )
-                raise UserException(message)
-        except exceptions.RefreshError as err:
-            message = 'Cannot connect to BigQuery.' \
-                ' Check your access token or refresh token.'
-            raise UserException(message)
-        except BQExceptions.BadRequest as err:
-            message = 'Cannot create dataset %s: %s' % (
-                dataset_name,
-                str(err)
-            )
-            raise UserException(message)
-
-        table_ref = dataset.table(table_definition['dbName'])
-        table = bigquery.Table(table_ref, columns_schema)
-
-        try:
-            bq_table = self.bigquery_client.get_table(table_ref)
-            table_exist = True
-            if incremental:
-                schema_mapper.is_table_definition_in_match_with_bigquery(
-                    columns_schema,
-                    bq_table
-                )
-            else:
-                self.bigquery_client.delete_table(table_ref)
-                table_exist = False
-        except BQExceptions.NotFound:
-            table_exist = False
-        except BQExceptions.BadRequest as err:
-            message = 'Cannot create table %s: %s' % (
-                table_definition['dbName'],
-                str(err)
-            )
-            raise UserException(message)
-
-        if not table_exist:
-            try:
-                self.bigquery_client.create_table(table)
-            except BQExceptions.BadRequest as err:
-                message = 'Cannot create table %s: %s' % (
-                    table_definition['dbName'],
-                    str(err)
-                )
-                raise UserException(message)
+        dataset = self.obtain_dataset(dataset_name)
+        table_reference = self.prepare_table(dataset, table_definition['dbName'], columns_schema, incremental)
 
         with open(csv_file_path, 'rb') as readable:
             job_config = bigquery.LoadJobConfig()
@@ -86,16 +103,22 @@ class Writer(object):
 
             job = self.bigquery_client.load_table_from_file(
                 readable,
-                table_ref,
+                table_reference,
                 job_config=job_config
             )
 
             return job
 
-    def write_table_sync(self, csv_file_path, dataset_name, table_definition,
-                         columns_schema, incremental=False,
-                         polling_max_retries=360,
-                         polling_delay=5):
+    def write_table_sync(
+            self,
+            csv_file_path: str,
+            dataset_name: str,
+            table_definition: dict,
+            columns_schema: list,
+            incremental: bool = False,
+            polling_max_retries: int = 360,
+            polling_delay: int = 5
+    ) -> None:
         job = self.write_table(
             csv_file_path,
             dataset_name,
