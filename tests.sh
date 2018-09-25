058656ad0d91570a7b86e9a318d029980f9e5018
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-09-25 23:45:28 +0200

    fix: flake8 allows longer lines

--- tests.sh
+++ tests.sh
@@ -4,5 +4,5 @@ set -e
 export KBC_DATA_DIR=/home/test/data
 cd /home/
 
-flake8 ./ --exclude=venv --statistics
+flake8 ./ --exclude=venv --statistics --max-line-length 120
 py.test --cov=google_bigquery_writer --cov-report term-missing
