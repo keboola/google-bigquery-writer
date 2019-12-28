ae08d76e98f1bb8d8690993ef576c3ac6f46b672
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2019-12-28 22:27:10 +0100

    tmp: test asserts

--- test/test_app.py
+++ test/test_app.py
@@ -109,7 +109,9 @@ class TestApp(GoogleBigQueryWriterTest):
         assert err == ''
         assert out == 'Loading table in.c-bucket.table1 into BigQuery ' \
             'as %s.table1\n' \
+            'Attempt: 1\n' \
             'Loading table in.c-bucket.table2 into BigQuery as %s.table2\n' \
+            'Attempt: 1\n' \
             'BigQuery Writer finished\n' % (
                 os.environ.get('BIGQUERY_DATASET'),
                 os.environ.get('BIGQUERY_DATASET')
@@ -223,7 +225,9 @@ class TestApp(GoogleBigQueryWriterTest):
         assert err == ''
         assert out == 'Loading table in.c-bucket.table1 into BigQuery ' \
             'as %s.table1\n' \
+            'Attempt: 1\n' \
             'Loading table in.c-bucket.table2 into BigQuery as %s.table2\n' \
+            'Attempt: 1\n' \
             'BigQuery Writer finished\n' % (
                 os.environ.get('BIGQUERY_DATASET'),
                 os.environ.get('BIGQUERY_DATASET')
