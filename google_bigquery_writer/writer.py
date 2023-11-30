6ac145588c3e02fb6731a8e3fbc9d261b81dd799
SgtMarmite <d.novotny31415@gmail.com>
2023-11-30 10:41:43 +0100

    set filesize limit to 4GB

--- google_bigquery_writer/writer.py
+++ google_bigquery_writer/writer.py
@@ -13,7 +13,7 @@ import time
 
 class Writer(object):
     REQUEST_TIMEOUT = 120  # Timeout in seconds
-    MAX_CHUNK_SIZE_MB = 400
+    MAX_CHUNK_SIZE_MB = 4000
     TEMP_PATH = '/home/data/temp'
 
     def __init__(self, bigquery_client: bigquery.Client):
