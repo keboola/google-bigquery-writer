66d130aad10daa6bc16354ff78e73db8401b09c6
SgtMarmite <d.novotny31415@gmail.com>
2023-12-01 10:28:07 +0100

    set threads dynamically

--- google_bigquery_writer/writer.py
+++ google_bigquery_writer/writer.py
@@ -13,6 +13,7 @@ from concurrent.futures import as_completed
 import backoff
 import math
 import os
+import psutil
 import subprocess
 import time
 
@@ -20,11 +21,11 @@ import time
 class Writer(object):
     REQUEST_TIMEOUT = 120  # Timeout in seconds
     MAX_CHUNK_SIZE_MB = 1_000
-    MAX_WORKERS = 4
     TEMP_PATH = '/home/data/temp'
 
     def __init__(self, bigquery_client: bigquery.Client):
         self.bigquery_client = bigquery_client
+        self.MAX_WORKERS = self._set_workers()
 
     def obtain_dataset(self, dataset_name: str) -> bigquery.Dataset:
         dataset_reference = DatasetReference(self.bigquery_client.project, dataset_name)
@@ -233,5 +234,13 @@ class Writer(object):
         except Exception as e:
             raise UserException(f"Error running cli_linux_amd64: {e}")
 
-    def _calculate_slices(self, size_mb, max_chunk_size_mb):
+    @staticmethod
+    def _calculate_slices(size_mb, max_chunk_size_mb):
         return math.ceil(size_mb / max_chunk_size_mb)
+
+    @staticmethod
+    def _set_workers() -> int:
+        workers = int(psutil.cpu_count() / psutil.cpu_count(logical=False))
+        print(f"Number of threads set to {workers}")
+        return workers
+
